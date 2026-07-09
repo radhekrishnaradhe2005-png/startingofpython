b3707078eb8f63b4d6dd908cb27cb3cc0c309999
vikash tomar <radhekrishnaradhe2005@gmail.com>
2026-07-09 22:35:43 +0530

    Add files via upload

---
+++ variableanddatatypes.py
@@ -0,0 +1,20 @@
+# this is the example of variable 
+'''a=3
+b= 34
+print(a+b)'''
+
+
+
+# now the datatypes
+# integer
+a=2 
+# floating number
+b=2.3
+# string
+c="vikash"
+print(a+b)
+print(c)
+# print("vikash is my name ")    
+
+
+
