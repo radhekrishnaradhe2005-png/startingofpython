f63261af12563815b0371287cafdcd44c7151c56
vikash tomar <radhekrishnaradhe2005@gmail.com>
2026-07-09 22:37:36 +0530

    Add files via upload

---
+++ practiceset2.py
@@ -0,0 +1,17 @@
+#write a  program to add two numbers 
+# a =2
+# b=1
+# print(a+b)
+
+
+#write a program to find remainder when a number is divided by z 
+# a = int (input("entre the first number "))
+# b = int (input("entre the second number "))
+# print(a%b)
+
+
+# use compartor operator to find whether the number is greater or smaller either than other number or not
+# a = 12 
+# b =32
+# if a >= b:
+# print(" i have  done this problem")
